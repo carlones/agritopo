52583f5ed0bda904cc8a67e630b37ba2681e0c81
Carlos Eduardo Nissola Migliavacca <carlos.migliavacca@gmail.com>
2018-11-17 21:02:13 -0200

    #000: Removido ordenação de elementos e corrigido para não gerar erro na leitura de mapas, colocando o zoom e ponto central padrão quando necessário

--- app/src/main/java/br/com/neogis/agritopo/model/MapaTiles.java
+++ app/src/main/java/br/com/neogis/agritopo/model/MapaTiles.java
@@ -28,22 +28,24 @@ public class MapaTiles {
             //SQLiteDatabase db_mapa = SQLiteDatabase.openOrCreateDatabase(arquivo.toString(), Context.MODE_PRIVATE, null);
             SQLiteDatabase db_mapa = SQLiteDatabase.openDatabase(arquivo.toString(), null, Context.MODE_PRIVATE);
             Cursor cursor = db_mapa.rawQuery("SELECT value FROM metadata WHERE name IN ('minzoom', 'maxzoom') ORDER BY value", null);
-            cursor.moveToFirst();
-            zoomMinimo = cursor.getInt(0);
-            cursor.moveToNext();
-            zoomMaximo = cursor.getInt(0);
-            Log.d("Agritopo", String.format("níveis de zoom do arquivo: %d, %d", zoomMin, zoomMax));
-            cursor.close();
+            if (cursor.moveToNext()) {
+                zoomMinimo = cursor.getInt(0);
+                cursor.moveToNext();
+                zoomMaximo = cursor.getInt(0);
+                Log.d("Agritopo", String.format("níveis de zoom do arquivo: %d, %d", zoomMin, zoomMax));
+                cursor.close();
+            }
             cursor = db_mapa.rawQuery("SELECT value FROM metadata WHERE name = 'bounds'", null);
-            cursor.moveToFirst();
-            String comma_separated_bounds = cursor.getString(0);
-            String[] bounds_parts = comma_separated_bounds.split(",");
-            double lat = (Double.parseDouble(bounds_parts[1]) + Double.parseDouble(bounds_parts[3])) / 2.0;
-            double lon = (Double.parseDouble(bounds_parts[0]) + Double.parseDouble(bounds_parts[2])) / 2.0;
-            Log.d("Agritopo", String.format("posições calculadas: %f, %f", lat, lon));
-            cursor.close();
+            if (cursor.moveToNext()) {
+                String comma_separated_bounds = cursor.getString(0);
+                String[] bounds_parts = comma_separated_bounds.split(",");
+                double lat = (Double.parseDouble(bounds_parts[1]) + Double.parseDouble(bounds_parts[3])) / 2.0;
+                double lon = (Double.parseDouble(bounds_parts[0]) + Double.parseDouble(bounds_parts[2])) / 2.0;
+                Log.d("Agritopo", String.format("posições calculadas: %f, %f", lat, lon));
+                pontoCentralizado = new GeoPoint(lat, lon);
+                cursor.close();
+            }
             db_mapa.close();
-            pontoCentralizado = new GeoPoint(lat, lon);
         } finally {
             this.zoomMin = zoomMinimo;
             this.zoomMax = zoomMaximo;
