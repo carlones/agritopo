d7e26dfba6ac0b28e37f70407151738cd80c6493
Carlos Eduardo Nissola Migliavacca <carlos.migliavacca@outlook.com>
2020-02-22 14:53:55 +0000

    Revert "#334 Alterar cadastro de ponto para permitir editar o local geográfico"

--- app/src/main/java/br/com/neogis/agritopo/activity/ElementoDetailActivity.java
+++ app/src/main/java/br/com/neogis/agritopo/activity/ElementoDetailActivity.java
@@ -1,6 +1,5 @@
 package br.com.neogis.agritopo.activity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,21 +13,16 @@ import android.view.View;
 import java.util.ArrayList;
 
 import br.com.neogis.agritopo.R;
-import br.com.neogis.agritopo.dao.tabelas.Classe;
-import br.com.neogis.agritopo.dao.tabelas.ClasseDao;
 import br.com.neogis.agritopo.dao.tabelas.Elemento;
 import br.com.neogis.agritopo.dao.tabelas.ElementoDao;
 import br.com.neogis.agritopo.dao.tabelas.ElementoDaoImpl;
-import br.com.neogis.agritopo.dao.tabelas.Fabricas.FabricaClasseDao;
 import br.com.neogis.agritopo.dao.tabelas.Fabricas.FabricaElementoDao;
 import br.com.neogis.agritopo.dao.tabelas.Fabricas.FabricaTipoElementoDao;
 import br.com.neogis.agritopo.dao.tabelas.TipoElemento;
 import br.com.neogis.agritopo.dao.tabelas.TipoElementoDao;
 import br.com.neogis.agritopo.dao.tabelas.TipoElementoDaoImpl;
 import br.com.neogis.agritopo.fragment.ElementoDetailFragment;
-import br.com.neogis.agritopo.fragment.ElementoDetailGeomFragment;
 
-import static br.com.neogis.agritopo.dao.tabelas.ClasseEnum.PONTO;
 import static br.com.neogis.agritopo.utils.Constantes.ARG_CLASSEID;
 import static br.com.neogis.agritopo.utils.Constantes.ARG_ELEMENTOID;
 import static br.com.neogis.agritopo.utils.Constantes.ARG_ELEMENTO_CENTRALIZAR;
@@ -44,52 +38,37 @@ import static br.com.neogis.agritopo.utils.Constantes.ARG_TIPOELEMENTOID;
  */
 public class ElementoDetailActivity extends AppCompatActivity {
     private ElementoDetailFragment fragment;
-    private ElementoDetailGeomFragment fragmentGeom;
-    private boolean geomAtivado;
     private int elementoId;
     private String geometria;
     private int posicao_lista;
     private Elemento mItem;
     private String tagFragmento = "tag_fragmento_elemento";
-    private String tagFragmentoGeom = "tag_fragmento_elemento_geom";
 
-    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elemento_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
+
         elementoId = getIntent().getIntExtra(ARG_ELEMENTOID, 0);
         int tipoelementoId = getIntent().getIntExtra(ARG_TIPOELEMENTOID, 0);
         int classeId = getIntent().getIntExtra(ARG_CLASSEID, 0);
         geometria = getIntent().getStringExtra(ARG_GEOMETRIA);
         posicao_lista = getIntent().getIntExtra(ARG_POSICAO_LISTA, -1);
 
-        ClasseDao classeDao = FabricaClasseDao.Criar(getBaseContext());
-        Classe classe = classeDao.get(classeId);
-        geomAtivado = false;
         FloatingActionButton btnElementoSalvar = (FloatingActionButton) findViewById(R.id.btnElementoSalvar);
         btnElementoSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (geomAtivado) {
-                    geomAtivado = false;
-                    getFragmentManager().beginTransaction()
-                            .hide(fragmentGeom)
-                            .show(fragment)
-                            .commit();
-                    geometria = fragmentGeom.getGeometria().toString();
-                    fragment.setGeometria(fragmentGeom.getGeometria());
-                } else {
-                    gravarElemento();
-                    Intent intent = new Intent();
-                    intent.putExtra(ARG_ELEMENTOID, mItem.getElementoid());
-                    intent.putExtra(ARG_POSICAO_LISTA, posicao_lista);
-                    setResult(RESULT_OK, intent);
-                    finish();
-                    onBackPressed();
-                }
+                gravarElemento();
+                //excluirImagens();
+                Intent intent = new Intent();
+                intent.putExtra(ARG_ELEMENTOID, mItem.getElementoid());
+                intent.putExtra(ARG_POSICAO_LISTA, posicao_lista);
+                setResult(RESULT_OK, intent);
+                finish();
+                onBackPressed();
             }
         });
         FloatingActionButton btnElementoFocar = (FloatingActionButton) findViewById(R.id.btnElementoFocar);
@@ -107,21 +86,6 @@ public class ElementoDetailActivity extends AppCompatActivity {
         });
         btnElementoFocar.setVisibility((elementoId == 0 ? View.INVISIBLE : View.VISIBLE));
 
-        FloatingActionButton btnElementoAlterarLocal = (FloatingActionButton) findViewById(R.id.btnElementoAlterarLocal);
-        btnElementoAlterarLocal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!geomAtivado) {
-                    geomAtivado = true;
-                    getFragmentManager().beginTransaction()
-                            .hide(fragment)
-                            .show(fragmentGeom)
-                            .commit();
-                }
-            }
-        });
-        btnElementoAlterarLocal.setVisibility((classe.getClasseEnum() == PONTO ? View.VISIBLE : View.INVISIBLE));
-
         // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -138,42 +102,36 @@ public class ElementoDetailActivity extends AppCompatActivity {
         // http://developer.android.com/guide/components/fragments.html
         //
         if (savedInstanceState == null) {
-
-            Bundle arguments1 = new Bundle();
-            arguments1.putInt(ARG_ELEMENTOID, elementoId);
-            arguments1.putInt(ARG_TIPOELEMENTOID, tipoelementoId);
-            arguments1.putInt(ARG_CLASSEID, classeId);
-            arguments1.putString(ARG_GEOMETRIA, geometria);
-
+            // Create the detail fragment and add it to the activity
+            // using a fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putInt(ARG_ELEMENTOID, elementoId);
+            arguments.putInt(ARG_TIPOELEMENTOID, tipoelementoId);
+            arguments.putInt(ARG_CLASSEID, classeId);
+            arguments.putString(ARG_GEOMETRIA, geometria);
             fragment = new ElementoDetailFragment();
-            fragment.setArguments(arguments1);
+            fragment.setArguments(arguments);
             fragment.setListaImagens(new ArrayList<String>());
             fragment.setListaImagensExcluir(new ArrayList<String>());
-
-            Bundle arguments2 = new Bundle();
-            arguments2.putInt(ARG_ELEMENTOID, elementoId);
-            arguments2.putInt(ARG_TIPOELEMENTOID, tipoelementoId);
-            arguments2.putInt(ARG_CLASSEID, classeId);
-            arguments2.putString(ARG_GEOMETRIA, geometria);
-            fragmentGeom = new ElementoDetailGeomFragment();
-            fragmentGeom.setArguments(arguments2);
-
             getFragmentManager().beginTransaction()
                     .add(R.id.elemento_detail_container, fragment, tagFragmento)
-                    .add(R.id.elemento_detail_geom_container, fragmentGeom, tagFragmentoGeom)
-                    .hide(fragmentGeom)
                     .commit();
         } else {
             fragment = (ElementoDetailFragment) getFragmentManager().getFragment(savedInstanceState, tagFragmento);
-            fragmentGeom = (ElementoDetailGeomFragment) getFragmentManager().getFragment(savedInstanceState, tagFragmentoGeom);
+        }
+    }
+
+    private void excluirImagens() {
+        for (String image : fragment.getListaImagensExcluir()) {
+            getContentResolver().delete(Uri.parse(image), null, null);
         }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+
         getFragmentManager().putFragment(outState, tagFragmento, fragment);
-        getFragmentManager().putFragment(outState, tagFragmentoGeom, fragmentGeom);
     }
 
     @Override
